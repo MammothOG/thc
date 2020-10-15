ae05cffe198e7295469b63f32b73d2eed6f472e3
MammothOG <felix.capuano@gmail.com>
2020-10-15 19:24:52 +0200

    youtube request testing

--- client.js
+++ client.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
 
 const data = {
-  "name": "felix",
-  "pseudo": "mammoth"
+  content: {
+    service: "youtube", // local, twitch...
+    path: "https://www.youtube.com/watch?v=Kw5tC5nQMRY&t=102s&ab_channel=TheCodingTrain"
+  },
+  params: {
+    status: "stop" // run
+  }
 }
 
-axios
-  .post('http://127.0.0.1:3000/api', data)
-  .catch((error) => {
-    console.error(error);
-  })
-
-
+axios.post('http://127.0.0.1:3000/api', data)
 // using web
 //fetch("/api", options)
