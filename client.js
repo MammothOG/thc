ca594f911d1ccbd15cbedc6cb93ce37ce2ee9877
MammothOG <felix.capuano@gmail.com>
2020-10-21 11:29:51 +0200

    catch error and get request

--- client.js
+++ client.js
@@ -4,14 +4,23 @@ const data = {
   session: 0, // 0(New session), 1, 2...
   status: "play", // pause
   move: "next", // previous, start, end
-  add: {
+  media: {
     service: "youtube", // local, twitch...
-    time: 0, // in second
+    start_time: 0, // in second
     file: "",
     http: "",
   },
 }
 
 axios.post('http://127.0.0.1:3000/remote', data)
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error.response.status);
+    console.log(error.response.statusText);
+  })
 // using web
 //fetch("/api", options)
