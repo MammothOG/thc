2eccd4a77b0d15e596cfb76d8d2651805210ac95
MammothOG <felix.capuano@gmail.com>
2020-11-02 22:25:25 +0100

    start creating button to add media on yt

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ console.log("background is running");
 let ip = "localhost";
 let port = 3000;
 
-const addr = (ip, port) => { return "http://"+ip+":"+port+"/remote" }
+const addr = (ip, port) => { return "http://" + ip + ":" + port + "/remote" }
 
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
--- youtube.js
+++ youtube.js
@@ -1,1 +1,6 @@
 console.log("youtube content loaded");
+
+let bar = document.getElementById("top-level-buttons");
+let addButton = document.createButton("button");
+addButton.innerHTML = "add";
+bar.appendChild(addButton);
