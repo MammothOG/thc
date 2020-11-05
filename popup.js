0d39305cadc7488bb86b5706175206a40336c5e4
MammothOG <felix.capuano@gmail.com>
2020-11-05 10:22:59 +0100

    add button action

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let buttonPlay = document.getElementById('play');
+    let buttonPlayPause = document.getElementById('play');
     let buttonPrevious = document.getElementById('previous');
     let buttonNext = document.getElementById('next');
+    let buttonClearPlaylist = document.getElementById('clear-playlist');
+    let buttonStop = document.getElementById('stop');
 
-    function onPlay() {
+    function onPlayPause() {
         if (buttonPlay.innerHTML === "pause") {
             chrome.runtime.sendMessage({ action: "pause" });
             buttonPlay.innerHTML = "play";
@@ -14,15 +16,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function onNext() {
-        chrome.runtime.sendMessage({ action: "next"});
-    }
+    function onNext() { chrome.runtime.sendMessage({ action: "next"}); }
+    function onPrevious() { chrome.runtime.sendMessage({ action: "previous"}); } 
+    function onStop() { chrome.runtime.sendMessage({ action: "stop"}); } 
+    function onClearPlaylist() { chrome.runtime.sendMessage({ action: "clearplaylist"}); } 
 
-    function onPrevious() {
-        chrome.runtime.sendMessage({ action: "previous"});
-    }
-
-    buttonPlay.addEventListener('click', onPlay, false)
-    buttonPrevious.addEventListener('click', onPrevious, false)
-    buttonNext.addEventListener('click', onNext, false)
+    buttonPlayPause.addEventListener('click', onPlayPause, false);
+    buttonPrevious.addEventListener('click', onPrevious, false);
+    buttonNext.addEventListener('click', onNext, false);
+    buttonStop.addEventListener('click', onStop, false);
+    buttonClearPlaylist.addEventListener('click', onClearPlaylist, false);
+    
 })
